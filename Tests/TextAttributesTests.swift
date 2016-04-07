21780df5cf0bcdd66114d6124d5fe99e13c7e8d3
delba <delba@users.noreply.github.com>
2016-04-07 17:42:19 +0200

    Share the TextAttributesTests scheme

--- Tests/TextAttributesTests.swift
+++ Tests/TextAttributesTests.swift
@@ -11,15 +11,4 @@ import UIKit
 @testable import TextAttributes
 
 class TextAttributesTests: XCTestCase {
-    func testExample() {
-        // let style: NSMutableParagraphStyle = NSMutableParagraphStyle.defaultParagraphStyle()
-        // let mut = NSMutableParagraphStyle.setParagraphStyle(style)
-        
-        let attrs = TextAttributes()
-        attrs.font = UIFont(name: "HelveticaNeue", size: 12)
-        attrs.foregroundColor = .blackColor()
-        
-        attrs.font(name: "HelveticaNeue", size: 12)
-            .foregroundColor(.blackColor())
-    }
 }
